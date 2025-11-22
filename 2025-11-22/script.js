3f42783269df9dffddb0f494945e5ec37cfc21c9
Prathamesh Sawant <prathameshsawant201@gmail.com>
2025-11-22 23:50:41 +0530

    todays code

---
+++ 2025-11-22/script.js
@@ -0,0 +1,37 @@
+// console.log("hello world")
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(5,3));
+
+// factorial function
+
+// function factorial(n){
+//     if(n===1) return 1;
+//     return n * factorial(n-1);
+// }
+// console.log(factorial(10));
+
+// how to create object
+
+// let person = {name:"Prathamesh", age:23};
+
+// console.log(person);
+
+// function boys(name,age){
+//     this.name = name,
+//     this.age = age
+// }
+
+// let b1 = new boys("Ramjan", 22);
+// console.log(b1);
+
+let nums = [1,2,3,4];
+
+let mul = nums.reduce((acc,val) => acc-val,0);
+console.log(mul);
+
+
+
